c092816cf036f650a5305255333f1ecc16b7b96c
Vishakh Aswani Kumar <vishakh@Vishakhs-MacBook-Pro.local>
2021-07-01 14:32:23 -0500

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+
+//Import Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+//Import Pages
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const URL = "https://express-and-react-backend.herokuapp.com/";
+    return (
+        <div className="App">
+            <Header />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/projects">
+                    <Projects URL={URL} />
+                </Route>
+                <Route path="/about">
+                    <About URL={URL} />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = (props) => {
+    //inline styling
+    const navStyle = {
+        display: "flex",
+        jusityContect: "space-around",
+        border: "3px solid black",
+        padding: "8px",
+        width: "90%",
+        margin: "auto",
+    };
+    return (
+        <header>
+            <h1>My Portfolio Page</h1>
+            <nav style={navStyle}>
+                <Link to="/">
+                    <div>HOME</div>
+                </Link>
+                <Link to="/about">
+                    <div>ABOUT</div>
+                </Link>
+                <Link to="/projects">
+                    <div>PROJECTS</div>
+                </Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
