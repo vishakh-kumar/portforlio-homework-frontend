87b792a2419eb4d6da3fb628e87156207a378953
Vishakh Aswani Kumar <vishakh@Vishakhs-MacBook-Pro.local>
2021-07-01 16:15:21 -0500

    add styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Footer = (props) => {
     return (
-        <div>
-            <h1>footer</h1>
+        <div className="footer">
+            <p>
+                <i class="far fa-copyright"></i> Vishakh Portfolio
+                <Link to="https://www.instagram.com/vishakh_kumar/">
+                    <i className="fab fa-instagram"></i>
+                </Link>
+                <Link to="https://www.github.com/vishakh-kumar">
+                    <i class="fab fa-github"></i>
+                </Link>
+                <Link to="https://www.linkedin.com/in/vishakh-kumar/">
+                    <i class="fab fa-linkedin"></i>
+                </Link>
+            </p>
         </div>
     );
 };
